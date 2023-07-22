690ff7beb9ee5b2e1ac32650db8040a015e58489
Reinaldo "Naufrago" Moreno <djnaufrago@yandex.com>
2023-07-22 10:41:00 +0200

    Update wprestore.sh

--- wprestore.sh
+++ wprestore.sh
@@ -15,7 +15,14 @@ _SQLFILE="wp_db_xxxxxx.sql"
 # Database root password.
 _ROOTPASS="abc123"
 
-cd $_BACKUPS="/home/ubuntu/backups"
+# Make the user temporarily owner of the web directory.
+sudo chown -R $USER:www-data /var/www/
+
+# Delete the web directory.
+rm -rf $_WPDIRECTORY/*/
+
+# Copy the backup file to the web directory, unzip it and delete the copy.
+cd $_BACKUPS
 cp $_WPBKSITE $_WPDIRECTORY
 cd $_WPDIRECTORY
 tar -xzf $_WPBKSITE
@@ -28,8 +35,14 @@ DBUSER=$(wp config get DB_USER --path="$_WPDIRECTORY")
 DBPASS=$(wp config get DB_PASSWORD --path="$_WPDIRECTORY")
 DBHOST=$(wp config get DB_HOST --path="$_WPDIRECTORY")
 
+# Delete (if it exists) the database.
+mysql -u root -p$_ROOTPASS -se 'DROP DATABASE IF EXISTS $DBNAME;'
+
 # Create and restore the database.
 mysql -u root -p$_ROOTPASS -se 'CREATE DATABASE $DBNAME;'
 mysql -u root -p$_ROOTPASS $DBNAME < "$_BACKUPS"/"$_SQLFILE"
 mysql -u root -p$_ROOTPASS -se "GRANT ALL PRIVILEGES ON $DBNAME.* to 'DBUSER'@'$DBHOST' identified by '$DBPASS';"
 mysql -u root -p$_ROOTPASS -se 'FLUSH PRIVILEGES;'
+
+# Return the directory to the web user.
+sudo chown -R www-data:www-data /var/www/
